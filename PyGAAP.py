3f2466f810680b0130945bcd6003e722f61d2dcf
David Berdik <berdikd@duq.edu>
2019-10-22 18:46:03 -0400

    Basic setup for the CLI

--- PyGAAP.py
+++ PyGAAP.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import logging.config
 
+from backend.CLI import cliMain
+
 def main():
 	# Set logging configuration
 	logging.config.fileConfig('PyGAAPLogging.config')
@@ -8,6 +10,7 @@ def main():
 	
 	# TODO: A GUI-based version of PyGAAP. For now, it's all CLI.
 	logger.info("Starting CLI")
+	cliMain()
 
 if __name__=="__main__":
 	main()
---
+++ backend/CLI.py
@@ -0,0 +1,20 @@
+import argparse, sys
+
+def cliMain():
+	"""Main function for the PyGAAP CLI"""	
+	if len(sys.argv) < 2:
+		_parse_args(True)
+		
+	args = _parse_args()
+
+def _parse_args(empty=False):
+	"""Parse command line arguments"""
+	parser = argparse.ArgumentParser(description='Welcome to PyGAAP\u2014the Python Graphical Authorship Attribution Program')
+	parser.add_argument('-ee', '--experimentengine', metavar='csv-file', nargs=1, help="Specifies a CSV file for batch processing multiple experiments at once.")
+	# If no arguments specified, print help and completely exit.
+	if empty:
+		parser.print_help()
+		sys.exit(1)
+	
+	# Return parsed arguments.
+	return parser.parse_args()
