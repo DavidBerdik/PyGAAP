edabcb61836e362389342a43dd8e230d141ecd9c
David Berdik <berdikd@duq.edu>
2019-10-22 22:14:34 -0400

    The correct Python term is "list" and not "array"

--- generics/Canonicizer.py
+++ generics/Canonicizer.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 # An abstract Canonicizer class.
 class Canonicizer(ABC):
-	# Input is original text and output is canonicized text in the form of a character array.
+	# Input is original text and output is canonicized text in the form of a character list.
 	@abstractmethod
 	def process(self, procText):
 		pass
