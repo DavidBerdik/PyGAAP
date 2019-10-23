592adafecdedd888e14bdfdd8224f8bc02b8ac47
David Berdik <berdikd@duq.edu>
2019-10-23 16:49:25 -0400

    Why did I not use documentation-style comments when writing the generic Canoncizer class?

--- generics/Canonicizer.py
+++ generics/Canonicizer.py
@@ -2,12 +2,12 @@ from abc import ABC, abstractmethod
 
 # An abstract Canonicizer class.
 class Canonicizer(ABC):
-	# Input is original text and output is canonicized text in the form of a character list.
 	@abstractmethod
 	def process(self, procText):
+	'''Input is original text and output is canonicized text in the form of a character list.'''
 		pass
 		
-	# Returns the display name for the given canonicizer.
 	@abstractmethod
 	def displayName(self):
+		'''Returns the display name for the given canonicizer.'''
 		pass
