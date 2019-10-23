2fc3e252fb627f6ae9c737a4f913ac0304014e91
David Berdik <berdikd@duq.edu>
2019-10-22 21:56:50 -0400

    Forgot the @abstractmethod tags

--- generics/Canonicizer.py
+++ generics/Canonicizer.py
@@ -3,9 +3,11 @@ from abc import ABC, abstractmethod
 # An abstract Canonicizer class.
 class Canonicizer(ABC):
 	# Input is original text and output is canonicized text in the form of a character array.
+	@abstractmethod
 	def process(self, procText):
 		pass
 		
 	# Returns the display name for the given canonicizer.
+	@abstractmethod
 	def displayName(self):
 		pass
