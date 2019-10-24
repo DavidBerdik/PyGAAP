90fe7e2f98ea52d93e1e4844ebb7fef4fd1834f9
David Berdik <berdikd@duq.edu>
2019-10-24 13:53:46 -0400

    Added function for setting the Document object's EventSet value

--- backend/Document.py
+++ backend/Document.py
@@ -6,6 +6,11 @@ class Document:
 	eventSet = None
 	
 	def __init__(self, author, title, text):
+		'''Document object constructor.'''
 		self.author = author
 		self.title = title
 		self.text = text
+		
+	def setEventSet(self, eventSet)
+		'''Sets the eventSet list value.'''
+		self.eventSet = eventSet
