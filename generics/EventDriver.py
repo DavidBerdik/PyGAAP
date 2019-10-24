e9bf0d027a0ece43fd70ff70afba04848f226a11
David Berdik <berdikd@duq.edu>
2019-10-24 16:25:15 -0400

    Removed references to canonicizers from the EventDriver generic class. They were added by accident and we do not need or want them.

--- generics/EventDriver.py
+++ generics/EventDriver.py
@@ -2,20 +2,6 @@ from abc import ABC, abstractmethod
 
 # An abstract EventDriver class.
 class EventDriver(ABC):
-	canonicizers = []
-	
-	def addCanonicizer(self, canonicizer):
-		'''Adds the given canonicizer to the list of canonicizers.'''
-		self.canonicizers.append(canonicizer)
-		
-	def removeCanonicizer(self, canonicizer):
-		'''Removes the given canonicizer from the list of canonicizers.'''
-		self.canonicizers.remove(canonicizer)
-		
-	def getCanonicizers(self)
-		'''Returns the list of canonicizers.'''
-		return self.canonicizers
-		
 	@abstractmethod
 	def displayName(self):
 		'''Returns the display name for the given event driver.'''
