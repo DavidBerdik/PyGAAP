9c784906357f950dc95f403cb6491db2f654542c
David Berdik <berdikd@duq.edu>
2019-10-23 22:21:34 -0400

    Added abstract DistanceFunction class

---
+++ generics/DistanceFunction.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+# An abstract DistanceFunction class.
+class DistanceFunction(ABC):
+	@abstractmethod
+	def distance(self, unknownHistogram, knownHistogram):
+	'''Input is the unknown and known histograms and output is the resulting distance calculation.'''
+		pass
+		
+	@abstractmethod
+	def displayName(self):
+		'''Returns the display name for the given distance function.'''
+		pass
