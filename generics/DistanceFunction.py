145457456cfbde21011eb7da8d32b93718632753
David Berdik <berdikd@duq.edu>
2019-11-01 17:52:41 -0400

    Removed unnecessary "self" from distance function in abstract DistanceFunction class

--- generics/DistanceFunction.py
+++ generics/DistanceFunction.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 # An abstract DistanceFunction class.
 class DistanceFunction(ABC):
 	@abstractmethod
-	def distance(self, unknownHistogram, knownHistogram):
+	def distance(unknownHistogram, knownHistogram):
 		'''Input is the unknown and known histograms and output is the resulting distance calculation.'''
 		pass
 		
