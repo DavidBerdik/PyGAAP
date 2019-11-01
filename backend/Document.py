73eb1d1b1fbc210984d95bf315751487300598d2
David Berdik <berdikd@duq.edu>
2019-11-01 14:34:31 -0400

    Added filepath field to Document class

--- backend/Document.py
+++ backend/Document.py
@@ -4,12 +4,14 @@ class Document:
 	title = ""
 	text = ""
 	eventSet = None
+	filepath = ""
 	
-	def __init__(self, author, title, text):
+	def __init__(self, author, title, text, filepath):
 		'''Document object constructor.'''
 		self.author = author
 		self.title = title
 		self.text = text
+		self.filepath = filepath
 		
 	def setEventSet(self, eventSet):
 		'''Sets the eventSet list value.'''
