11dc8e5223b869a763c13b396c40c6325c09bffb
David Berdik <berdikd@duq.edu>
2019-11-01 13:59:10 -0400

    Added function for setting the distance function used by the analysis driver

--- generics/AnalysisDriver.py
+++ generics/AnalysisDriver.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 
 # An abstract AnalysisDriver class.
 class AnalysisDriver(ABC):
+	distance = None
+	
 	@abstractmethod
 	def train(self, knownDocuments):
 		'''Train a model on the knownDocuments.'''
@@ -16,3 +18,7 @@ class AnalysisDriver(ABC):
 	def displayName():
 		'''Returns the display name for the given analysis method.'''
 		pass
+		
+	def setDistanceFunction(self, distance)
+		'''Sets the distance function to be used by the analysis driver.'''
+		self.distance = distance
