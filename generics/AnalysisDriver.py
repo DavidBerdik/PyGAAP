d44571afd3d1b6f998a9350580f0b13b522a1c39
David Berdik <berdikd@duq.edu>
2019-11-01 17:52:01 -0400

    Implemented Centroid Driver

--- generics/AnalysisDriver.py
+++ generics/AnalysisDriver.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import backend.Histograms as histograms
 
 # An abstract AnalysisDriver class.
 class AnalysisDriver(ABC):
@@ -22,3 +23,20 @@ class AnalysisDriver(ABC):
 	def setDistanceFunction(self, distance):
 		'''Sets the distance function to be used by the analysis driver.'''
 		self.distance = distance
+		
+class CentroidDriver(AnalysisDriver):
+	authorHistograms = None
+	
+	def train(self, knownDocuments):
+		'''Get a mean normalized histogram for each known author.'''
+		self.authorHistograms = histograms.generateKnownDocsMeanHistograms(histograms.generateKnownDocsNormalizedHistogramSet(knownDocuments))
+		
+	def analyze(self, unknownDocument):
+		'''Compare a normalized histogram of unknownDocument against the normalized known document histograms and return a dictionary of distances.'''
+		results = dict()
+		for author, knownHist in self.authorHistograms.items():
+			results[author] = self.distance.distance(histograms.normalizeHistogram(histograms.generateAbsoluteHistogram(unknownDocument)), knownHist)
+		return results
+	
+	def displayName():
+		return "Centroid Driver"
