2f9f30fe3797b989a898785e6bc173380aec09df
David Berdik <berdikd@duq.edu>
2019-11-02 18:17:42 -0400

    Print version number on PyGAAP start

--- PyGAAP.py
+++ PyGAAP.py
@@ -3,6 +3,7 @@ from backend.CLI import cliMain
 
 def main():	
 	# TODO: A GUI-based version of PyGAAP. For now, it's all CLI.
+	print("PyGAAP v1.0.0 alpha 1\nby David Berdik")
 	cliMain()
 
 if __name__=="__main__":
