1fc05bd0c635f27bc80cda7582c59e948f8eda40
Jiaojiao Gao <gdreamlend@gmail.com>
2019-12-03 16:40:35 -0500

    add functions to deal with Chinese

--- generics/Canonicizer.py
+++ generics/Canonicizer.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import jieba
+
 
 # An abstract Canonicizer class.
 class Canonicizer(ABC):
@@ -19,3 +21,35 @@ class NormalizeWhitespace(Canonicizer):
 
 	def displayName():
 		return "Normalize Whitespace"
+		
+class WordSegmentationForChinese(Canonicizer):
+	def process(self, procText):
+		
+		'''
+		# different cut modes
+		segfull_list = jieba.cut("content here", cut_all=True) #Full mode
+		segdef_list = jieba.cut(""content here"", cut_all=False) #Default Mode
+		seg_search_list = jieba.cut_for_search(""content here"")  # search mode
+		'''
+		#content = open(procText,"rb").read()
+		words = " ".join(jieba.cut(procText, cut_all=False)) #Default Mode
+		#log_f = open(outputText,"wb")
+		#log_f.write(words.encode('utf-8'))
+		return words
+	def displayName():
+		return "Word Segmentation For Chinese"
+		
+class TokenizationChinese(Canonicizer)
+	def process(self, procText):
+		'''Remove characters or words with these kinds of part of speech'''
+		stop_flag = ['x', 'c', 'u','d', 'p', 't', 'uj', 'm', 'f', 'r']
+		result = []
+		#filename = procText
+		#text = open(procText,"rb").read()
+		words = pseg.cut(procText)
+		for word, flag in words:
+			if flag not in stop_flag:
+				result.append(word)
+		return result
+	def displayName():
+		return "Tokenization For Chinese"
