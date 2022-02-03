6fd3f1d13083bf2a0462f96be22705f076dfa21d
Michael <79772638+Michaeljfang@users.noreply.github.com>
2022-02-02 21:52:26 -0500

    20220202 New canonicizers
    Added some new canonicizers:
    Unify Case,
    Strip Punctuation,
    Strip Numbers,
    Punctuation separator,
    Strip Alphanumeric,
    Strip Null Characters.

--- generics/Canonicizer.py
+++ generics/Canonicizer.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from tokenize import String
+import re
+from xml.etree.ElementTree import canonicalize
 
 # An abstract Canonicizer class.
 class Canonicizer(ABC):
@@ -26,3 +29,71 @@ class NormalizeWhitespace(Canonicizer):
 
 	def displayDescription():
 		return "Converts all whitespace characters (newline, space and tab) to a single space.  Uses Java Character.isWhitespace for classification."
+
+class UnifyCase(Canonicizer):
+	def process(self, procText):
+		"""Convert procText to lower case"""
+		return procText.lower()
+	
+	def displayName():
+		return "Unify Case"
+
+	def displayDescription():
+		return "Converts all text to lower case."
+
+class StripPunctuation(Canonicizer):
+	def process(self, procText):
+		"""Gets rid of punctuation characters"""
+		return ''.join([char for char in procText if char not in ",.?!\"'`;:-()&$"])
+	
+	def displayDescription():
+		return 'Strip all punctuation characters (,.?!"\'`;:-()&$) from the text.'
+
+	def displayName():
+		return "Strip Punctuation"
+
+class StripNumbers(Canonicizer):
+	def process(self, procText):
+		"""Converts each digit string to a single zero."""
+		regex_match=re.compile("0+")
+		procText=''.join(["0" for char in procText if char in "0123456789"])
+		return re.subn(regex_match, "0", procText)[0]
+
+	def displayDescription():
+		return "Converts each digit strings to a single 0"
+
+	def displayName():
+		return "Strip Numbers"
+
+class PunctuationSeparator(Canonicizer):
+	def process(self, procText):
+		"""Adds whitespaces before and after punctuations."""
+		return ''.join([" "+char+" " if char in ",.?!\"'`;:-()&$" else char for char in procText])
+	
+	def displayDescription():
+		return "Adds whitespaces before and after punctuations."
+	
+	def displayName():
+		return "Punctuation Separator"
+
+class StripAlphanumeric(Canonicizer):
+	def process(self, procText):
+		"""Strips all non-whitespace, non-punctuation marks."""
+		return ''.join([char for char in procText if char in " ,.?!\"'`;:-()&$"])
+
+	def displayDescription():
+		return "Strips all non-whitespace, non-punctuation marks. i.e. leaves only white spaces and punctuation marks."
+	
+	def displayName():
+		return "Strip Alpha-numeric"
+
+class StripNullCharacters(Canonicizer):
+	def process(self, procText):
+		return ''.join([char for char in procText if char!="\0"])
+
+	def displayDescription():
+		return "Strips all 0x00 from the text."
+	
+	def displayName():
+		return "Strip Null Characters"
+
